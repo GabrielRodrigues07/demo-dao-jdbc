ecd091be2e194cdd1e94be07b345f9683fd1868a
Gabriel Rodrigues <grodrigues256@gmail.com>
2020-08-23 16:32:48 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: Seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Gabriel Rodrigues");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
